069692ed892cd8266d376f153970016e1c077104
angiecortez <angiecorteztay1@hotmail.com>
2018-06-19 08:21:34 -0500

    agregando funcion de users

--- src/data.js
+++ src/data.js
@@ -5,12 +5,12 @@ let Cohorts1 = "http://127.0.0.1:8887/data/cohorts.json"
 let btn = document.getElementById('btn');
 
 // btn.addEventListener('click', ()=>{ })
-let xmlhttp = new XMLHttpRequest();
+var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
-   if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-       let myArr = JSON.parse(xmlhttp.responseText);
-       myFunction(myArr);
-   }
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      let myArr = JSON.parse(xmlhttp.responseText);
+      myFunction(myArr);
+  }
 };
 xmlhttp.open("GET", urlUsers, true);
 xmlhttp.send();
@@ -20,27 +20,43 @@ cuerpo.innerHTML = '';
 for (let valor of arr){
 cuerpo.innerHTML += `
 <tr>
-
-    <td>${valor.name}</td>
-    <td>${valor.signupCohort}</td>
-    <td>${valor.role}</td>
-
-  </tr>
+   <td>${valor.name}</td>
+   <td>${valor.signupCohort}</td>
+   <td>${valor.role}</td>
+ </tr>
 `
 }
 }
 
-// let xmlhttp1 = new XMLHttpRequest();
-// xmlhttp.onreadystatechange = function() {
-//    if (xmlhttp1.readyState == 4 && xmlhttp1.status == 200) {
-//        let myArr = JSON.parse(xmlhttp1.responseText);
-//        myFunction(myArra);
-//    }
-// };
-// xmlhttp1.open("GET", urlProgress, true);
-// xmlhttp1.send();
-//
-// function myFunction(arra) {
+//progress no se ve en pantalla
+let xmlhttp1 = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+   if (xmlhttp1.readyState == 4 && xmlhttp1.status == 200) {
+       let myArr = JSON.parse(xmlhttp1.responseText);
+       myFunction1(myArra);
+   }
+};
+xmlhttp1.open("GET", urlProgress, true);
+xmlhttp1.send();
+
+function myFunction1(arra) {
+  const ids = Object.key(arra);
+  for (let i in ids){
+    let dataId = ids[i];
+    console.log(dataId);
+  }
+}
+//   // const partsObj = arra[ids[0]].intro.units['01-introduction'].parts;
+  //   console.log(partsObj)
+  //   const keysParts = Object.keys(partsObj)
+  //   console.log(keysParts)
+  //   console.log(partsObj[keysParts[0]].duration)
+  // //    let count = 0 ;
+  //    for (const valor of keysParts) {
+  //       count += partsObj[valor].completed;
+  //    }
+  //    console.log(count)
+// }
 // var objeto=arra[0];
 // var id =(Object.keys(objeto));
 // console.log(id[0]);
--- src/main.js
+++ src/main.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-let table = document.getElementById('response-container')
-
-
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-   if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-       let myArr = JSON.parse(xmlhttp.responseText);
-       myFunction(myArr);
-   }
-};
-xmlhttp.open("GET", urlUsers, true);
-xmlhttp.send();
-function myFunction(arr) {
-
-for (let i in arr){
-
-// infoContainer. = '<p> ' + arr[i].name +  '</p>';
-  console.log(arr[i].id);
-}
-
-}
-=======
 const btnContinue = document.getElementById("btnLogin"); 
 btnContinue.addEventListener("click", () => {
   const hidden = document.getElementById("container-login");
@@ -28,4 +5,3 @@ btnContinue.addEventListener("click", () => {
   hidden.style.display = "none";
   show.style.display = "block";
 });
->>>>>>> 00a84f2b0053072eed55b4d49d8e70e7133ee4d4
