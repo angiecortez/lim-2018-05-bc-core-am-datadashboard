ca1ee44398b4b470922ebac498945b6c6235829a
angiecortez <angiecorteztay1@hotmail.com>
2018-07-02 09:17:00 -0500

    [ADD]dataspec, mejorando la data

--- src/data.js
+++ src/data.js
@@ -16,19 +16,38 @@ let URLCohorts = '../data/cohorts.json';
 
 loadJSON(URLUsers, (userJSON) => {
   loadJSON(URLProgress, (progressJSON) => {
+    options.cohortData.users = userJSON;
+    options.cohortData.progress= progressJSON;
+
     // extraer los ids de los cursos que sean de lima
     let id = 'lim-2018-03-pre-core-pw';
-      console.log(computeUsersStats(userJSON, progressJSON, id));
-      // const computeStas = computeUsersStats(userJSON, progressJSON, id);
-      // console.log(sortUsers(computeUsersStats1, 'asc'));
+      const computeUsersStats1 = computeUsersStats(userJSON, progressJSON, id);
+      console.log(sortUsers(computeUsersStats1, 'asc'));
+      // filterUsers(computeUsersStats1, 'search');
 
 
     // usar sólo courses tiene un problema y es que en el JSON de cohorts no hay ningún ID para estos
     // loadJSON(URLCohorts, (cohortsJSON) => {
     //   let cohort = cohortsJSON.filter((cohort) => cohort.id === id)[0];
-    //   let courses = Object.keys(cohort.coursesIndex); });
+      // let courses = Object.keys(cohort.coursesIndex);
+
+     });
   });
-});
+// });
+
+
+//empieza// let id = 'lim-2018-03-pre-core-pw';
+// const listCohorts = (id) => {
+//     selectCohorts.innerHTML = "";
+//     let cohortsJsonVariable = [];
+//     //const traerJson = () => {
+//         loadJSON(URLCohorts, (cohortsJSON) => {
+//           let cohort = cohortsJSON.map((cohort)=> cohort.id === id);
+//           selectCohorts.innerHTML += "<option value='" + cohort + "'>" + cohort + "</option>"
+//         })
+// }//termina
+
+// Añadir comentarioContraer 
 
 const calculateCompletionPercentage = (progress) => {
   let progressElements = Object.values(progress);
@@ -38,7 +57,7 @@ const calculateCompletionPercentage = (progress) => {
   let totalSum = progressElements.reduce((sum, element) => {
     return sum + element.percent;
   }, 0);
-  return totalSum / (100 * progressElements.length) * 100; debugger
+  return totalSum / (100 * progressElements.length) * 100;
 }
 
 // [ [1, 2, 3], [4, 5, 6] ] -> [1, 2, 3, 4, 5, 6] se usara para concatenar dos arrays que haga en una línea
@@ -112,52 +131,62 @@ window.computeUsersStats = (users, progress, courses) => {
   });
 }
 
+//refactorizador que hara la funcion de menos a mas
+const ascendent = (words) => {
+  return array.sort((a,b) => a > b ? 1: -1)
+}
+//refactorizador que hara la funcion de mas a menos
+const descendent = (words) => {
+  return array.sort((a,b) => a < b ? 1: -1)
+}
+
 // let approved = students.filter(student => student.score >= 11);
+window.sortUsers = (users, orderBy, orderDirection)=> { //asc y desc
+    const users = users.sort((a, b) => (a.name > b.name ? 1 : -1));
+    console.log(a);
+//  if(orderBy === 'name' && orderDirection === 'asc'){
+//     users.sort((a, b) => (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1));
+//  }else if(orderBy === 'name' && orderDirection === 'desc'){
+//     users.sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1));
+// }
+// if(orderBy === 'porcentaje completitud total' orderDirection === 'asc'){
+//    const userPercent = users.sort((a, b) => (a.percent > b.percent ? 1 : -1));
+//  }else if (orderBy === 'porcentaje completitud total' orderDirection === 'desc') {
+//    const userPercent = users.sort((a, b) => (a.percent < b.percent ? 1 : -1));
+// }
+// if (orderBy === 'completedExercises' orderDirection === 'asc') {
+//    const userExercisesCompleted = users.sort((a, b) => (a.stats.exercises.completed > b.stats.exercises.completed ? 1 : -1));
+//  } else if (orderBy === 'completedExercises' orderDirection === 'desc') {
+//    const userExercisesCompleted = users.sort((a, b) => (a.stats.exercises.completed < b.stats.exercises.completed ? 1 : -1));
+// }
+// if (orderBy === 'completedQuizzes' orderDirection === 'asc') {
+//    const userReadTotal = users.sort((a, b) => (a.stats.quizzes.completed > b.stats.quizzes.completed ? 1 : -1));
+//  } else if (orderBy === 'completedQuizzes' orderDirection === 'desc') {
+//    const userReadTotal = users.sort((a, b) => (a.stats.quizzes.completed < b.stats.quizzes.completed ? 1 : -1));
+// }
+// if (orderBy === 'Promedio de quizzes' orderDirection === 'asc') {
+//    const scoreSum = users.sort((a, b) => (a.stats.quizzes.scoreSum > b.stats.quizzes.scoreSum ? 1 : -1));
+//  } else if (orderBy === 'Promedio de quizzes' orderDirection === 'desc') {
+//    const scoreSum = users.sort((a, b) => (a.stats.quizzes.scoreSum < b.stats.quizzes.scoreSum ? 1 : -1));
+// }
+// if (orderBy === 'completedReads' orderDirection === 'asc') {
+//    const userReadTotal = users.sort((a, b) => (a.stats.reads.completed > b.stats.reads.completed ? 1 : -1));
+//  } else if (orderBy === 'completedReads' orderDirection === 'desc') {
+//    const userReadTotal = users.sort((a, b) => (a.stats.reads.completed < b.stats.reads.completed ? 1 : -1));
+// }
+//  return users;
+}
 
-//funcion para poner los elementos de mas a menos o de menos a más
-// window.sortUsers(users, orderBy, orderDirection){
-
-// if (orderBy  === 'asc'){
-//   users.sort((a, b) => (a.name > b.name ? 1 : -1));
-// } else {
-//   users.sort((a, b) => (a.name < b.name ? 1 : -1));
+// window.filterUsers = (users, search) => {
+//   let filterByUsers = users.filter(user => (user.name.toUpperCase().indexOf(search.toUpperCase()) !== -1);
+//  return filterByUsers;
 // }
 
-// const userPercent = users.sort((a, b) => (a.percent > b.percent ? 1 : -1));
-// const userExercisesTotal = users.sort((a, b) => (a.stats.exercises.total > b.stats.exercises.total ? 1 : -1));
-// const userExercisesCompleted = users.sort((a, b) => (a.stats.exercises.completed > b.stats.exercises.completed ? 1 : -1));
-// const userExercisesPercent = users.sort((a, b) => (a.stats.exercises.percent > b.stats.exercises.percent ? 1 : -1));
-// const userReadTotal = users.sort((a, b) => (a.stats.reads.total > b.stats.reads.total ? 1 : -1));
-// const userReadTotal = users.sort((a, b) => (a.stats.reads.completed > b.stats.reads.completed ? 1 : -1));
-// const userReadTotal = users.sort((a, b) => (a.stats.reads.percent > b.stats.reads.percent ? 1 : -1));
-
-//// de mas a menos
-
-// const name = users.sort((a, b) => (a.name < b.name ? 1 : -1));
-// const userPercent = users.sort((a, b) => (a.percent < b.percent ? 1 : -1));
-// const userExercisesTotal = users.sort((a, b) => (a.stats.exercises.total < b.stats.exercises.total ? 1 : -1));
-// const userExercisesCompleted = users.sort((a, b) => (a.stats.exercises.completed < b.stats.exercises.completed ? 1 : -1));
-// const userExercisesPercent = users.sort((a, b) => (a.stats.exercises.percent < b.stats.exercises.percent ? 1 : -1));
-
-
-// }
-//funcion para buscar
-// window.filterUsers(users, search){
-//   let filterByUsers = users.filter(user => {
-//     return user.name.toUpperCase().indexOf(search) > -1;
-//   });
-//   return filterByUsers;
-// }
-
-// windowprocessCohortData(options){
-//   options:{
-//     cohort:
-//     cohortData:{
-//       users:
-//       progress:
-//     }
-//     orderBy:
-//     orderDirection:
-//     search:
+// window.processCohortData = (options)  => {
+//    let users = computeUsersStats (options.cohortData.users, options.cohortData.progress, options.cohort.coursesIndex);
+//    users = sortUsers (users, options.orderBy, options.orderDirection);
+//    if (options.search !== '') {
+//     filterUsers (users, options.search);
+//     };
+//    return users;
 //   }
-// }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,23 @@
-let section = document.getElementById('container-lab-view');
-let idAcorsh = document.getElementById('acorsh');
-let table = document.getElementById('table');
-let body = document.getElementById('cuerpo');
+function printList(allList) {
+    let lista = document.querySelector("#body")
+    lista.innerHTML = "";
+    allList.forEach((computeUsersStats1) => {
+        let listaConStats = `<tr>
+        <td class="name">${computeUsersStats1.name}</td>
+        <td class="percent">${computeUsersStats1.percent}</td>
+        <td class="exercisesCompleted">${computeUsersStats1.stats.exercises.completed} de ${userStats.stats.exercises.total}</td>
+        <td class="quizzesCompleted">${computeUsersStats1.stats.quizzes.completed}</td>
+        <td class="quizzesScoreAvg">${computeUsersStats1.stats.quizzes.scoreAvg}</td>
+        <td class="readsCompleted">${computeUsersStats1.stats.reads.completed}</td>
+        </tr>`
+        lista.innerHTML += listaConStats
+    })
+}
 
-
-const paragraph = document.createElement('p');
-const content = document.createTextNode('Promoción');
-
-paragraph.appendChild(content);
-
-document.body.appendChild(parrafo);
-
-
+let botonCohort = document.getElementById("btn")
+botonCohort.addEventListener("click", (event) => {
+    printList(computeUsersStats1);
+})
 
 let options:{
   cohort: '',
@@ -19,7 +25,49 @@ let options:{
     users: '',
     progress:'',
   }
-  orderBy:'',
-  orderDirection:'',
+  orderBy:'name',
+  orderDirection:'asc',
   search:''
+}
+
+const loadJSON = (url, callback) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.send();
+}
+
+let URLUsers = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+let URLProgress = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+let URLCohorts = '../data/cohorts.json';
+
+loadJSON(URLUsers, (userJSON) => {
+  loadJSON(URLProgress, (progressJSON) => {
+    options.cohortData.users = userJSON;
+    options.cohortData.progress= progressJSON;
+
+    // extraer los ids de los cursos que sean de lima
+    let id = 'lim-2018-03-pre-core-pw';
+      const computeUsersStats1 = computeUsersStats(userJSON, progressJSON, id);
+      console.log(sortUsers(computeUsersStats1, 'asc'));
+      // filterUsers(computeUsersStats1, 'search');
+    });
+ });
+
+loadJSON(URLCohorts, (cohortsJSON)=>{
+  let cohort = cohortsJSON.filter((cohort) => cohort.id))
+
+    let lima= cohort.startsWith('lim-')
+    let chile = cohort.startsWith('scl-')
+    let aqp= cohort.startsWith('aqp-')
+    let mex = cohort.startsWith('cdmx') || cohort.startsWith('gdl-')
+    let brasil = cohort.startsWith('spl-')
+
+    options.cohort = cohort;
+
+
 }
